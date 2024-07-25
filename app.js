fa8af9110d24dfbf18ab21dec69f934c00f118d9
Aryan Lad <aryanlad07@gmail.com>
2024-07-25 13:51:01 +0530

    Updated app.js with root route redirect

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const { required } = require("joi");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+//const dburl = "mongodb://127.0.0.1:27017/wanderlust";
 const dburl = process.env.ATLASDB_URL;
 
 main()
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 const store = MongoStore.create({
     mongoUrl : dburl,
     crypto : {
-        secret : process.env.SECRET,
+        secret : "mysupersecretcode",
     },
     touchAfter : 24 * 3600,
 });
@@ -56,7 +56,7 @@ store.on("error",()=>{
 
 const sessionOptions = {
     store,
-    secret : process.env.SECRET,
+    secret : "mysupersecretcode",
     resave : false,
     saveUninitialized : true,
     cookie : {
@@ -66,9 +66,9 @@ const sessionOptions = {
     },
 }
 
-// app.get("/",(req,res)=>{
-//     res.send("Hi i'm root");
-// });
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
